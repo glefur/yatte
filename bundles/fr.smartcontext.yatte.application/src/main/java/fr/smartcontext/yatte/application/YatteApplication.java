9bebce279e3c36579df94c32c678722ffdea2dfc
Goulwen Le Fur <goulwen.lefur@obeo.fr>
2015-11-19 17:45:54 +0100

    ADDED tp

--- bundles/fr.smartcontext.yatte.application/src/main/java/fr/smartcontext/yatte/application/YatteApplication.java
+++ bundles/fr.smartcontext.yatte.application/src/main/java/fr/smartcontext/yatte/application/YatteApplication.java
@@ -8,6 +8,7 @@ import java.nio.file.Paths;
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.eclipse.equinox.app.IApplication;
@@ -32,35 +33,42 @@ public class YatteApplication implements IApplication {
 	/* (non-Javadoc)
 	 * @see org.eclipse.equinox.app.IApplication#start(org.eclipse.equinox.app.IApplicationContext)
 	 */
-	public final Object start(IApplicationContext context) throws Exception {
-		context.applicationRunning();
-		applicationBundle = FrameworkUtil.getBundle(getClass());
-		BundleContext bundleContext = applicationBundle.getBundleContext();
-		
-		String[] arguments = (String[]) context.getArguments().get(IApplicationContext.APPLICATION_ARGS);
-		Options options = initOptions();
-		CommandLineParser parser = new BasicParser();
-		CommandLine cmdLine = parser.parse(options, arguments);
-		
-		ContextInitializer contextInitializer;
-		
-		ServiceReference<ContextInitializer> ciRef = bundleContext.getServiceReference(ContextInitializer.class);
-		if (ciRef != null) {
-			contextInitializer = bundleContext.getService(ciRef);
-		} else {
-			contextInitializer = new PropertiesContextInitializer();
-		}
-		
-		ProcessingContext processingContext = contextInitializer.initContext(bundleContext, cmdLine);
-		
-		ServiceReference<TemplateProcessor> templateProcessorRef = bundleContext.getServiceReference(TemplateProcessor.class);
-		TemplateProcessor templateProcessor = bundleContext.getService(templateProcessorRef);
-		
-		PrintWriter fos = new PrintWriter(new FileOutputStream(new File(cmdLine.getOptionValue(ApplicationParametersConstants.OUTPUT_OPTION_NAME))));
+	public final Object start(IApplicationContext context) {
+		Options options = null;
+		try {
+			context.applicationRunning();
+			applicationBundle = FrameworkUtil.getBundle(getClass());
+			BundleContext bundleContext = applicationBundle.getBundleContext();
 
-		templateProcessor.process(Paths.get(cmdLine.getOptionValue(ApplicationParametersConstants.TEMPLATE_OPTION_NAME)), processingContext, fos);
-		
-		fos.close();
+			String[] arguments = (String[]) context.getArguments().get(IApplicationContext.APPLICATION_ARGS);
+			options = initOptions();
+			CommandLineParser parser = new BasicParser();
+			CommandLine cmdLine = parser.parse(options, arguments);
+
+			ContextInitializer contextInitializer;
+
+			ServiceReference<ContextInitializer> ciRef = bundleContext.getServiceReference(ContextInitializer.class);
+			if (ciRef != null) {
+				contextInitializer = bundleContext.getService(ciRef);
+			} else {
+				contextInitializer = new PropertiesContextInitializer();
+			}
+
+			ProcessingContext processingContext = contextInitializer.initContext(bundleContext, cmdLine);
+
+			ServiceReference<TemplateProcessor> templateProcessorRef = bundleContext.getServiceReference(TemplateProcessor.class);
+			TemplateProcessor templateProcessor = bundleContext.getService(templateProcessorRef);
+
+			PrintWriter fos = new PrintWriter(new FileOutputStream(new File(cmdLine.getOptionValue(ApplicationParametersConstants.OUTPUT_OPTION_NAME))));
+
+			templateProcessor.process(Paths.get(cmdLine.getOptionValue(ApplicationParametersConstants.TEMPLATE_OPTION_NAME)), processingContext, fos);
+
+			fos.close();
+		} catch (Exception ex) {
+			System.err.println("Unable to launch the publisher.");
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp( "p2repoPublisher", options );
+		}
 		return IApplication.EXIT_OK;
 	}
 
