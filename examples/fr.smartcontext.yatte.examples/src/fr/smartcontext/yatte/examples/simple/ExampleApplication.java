b91dde2c45969941b788dc0aca4ab6cb59746f23
Goulwen Le Fur <goulwen.lefur@smartcontext.fr>
2015-12-06 15:23:57 +0100

    UPDATED:  - new project structure for differenciating simple and advanced samples  - Saved launch config for simple sample

--- examples/fr.smartcontext.yatte.examples/src/fr/smartcontext/yatte/examples/simple/ExampleApplication.java
+++ examples/fr.smartcontext.yatte.examples/src/fr/smartcontext/yatte/examples/simple/ExampleApplication.java
@@ -5,7 +5,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
-package fr.smartcontext.yatte.examples;
+package fr.smartcontext.yatte.examples.simple;
 
 import fr.smartcontext.yatte.application.YatteApplication;
 
--- examples/fr.smartcontext.yatte.examples/src/fr/smartcontext/yatte/examples/simple/Person.java
+++ examples/fr.smartcontext.yatte.examples/src/fr/smartcontext/yatte/examples/simple/Person.java
@@ -5,7 +5,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
-package fr.smartcontext.yatte.examples;
+package fr.smartcontext.yatte.examples.simple;
 
 /**
  * @author <a href="goulwen.lefur@gmail.com">Goulwen Le Fur</a>
